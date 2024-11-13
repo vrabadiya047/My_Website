6d9efb98ca7c68e009c9fa7f7e0e45cb590d44f5
Vivek Rabadiya <vivek.rabadiya@cti-consulting.de>
2024-11-13 19:12:51 +0100

    index.html

---
+++ forms/contact.php
@@ -0,0 +1,28 @@
+<?php
+// Replace with your email
+$to = 'vivekrabadiya54@gmail.com';
+$subject = 'New Message from Contact Form';
+
+// Get form data
+$name = htmlspecialchars($_POST['name']);
+$email = htmlspecialchars($_POST['email']);
+$subjectField = htmlspecialchars($_POST['subject']);
+$message = htmlspecialchars($_POST['message']);
+
+// Email content
+$body = "Name: $name\n";
+$body .= "Email: $email\n";
+$body .= "Subject: $subjectField\n\n";
+$body .= "Message:\n$message";
+
+// Email headers
+$headers = "From: $email\r\n";
+$headers .= "Reply-To: $email\r\n";
+
+// Send email
+if(mail($to, $subject, $body, $headers)) {
+    echo 'success';
+} else {
+    echo 'error';
+}
+?>
